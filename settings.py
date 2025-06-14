aeddfc40bfe89cd6c482785dd2018df1b05c7f26
Will <xiaodown@gmail.com>
2025-06-13 17:02:20 -0700

    Adding explanations to settings.py

--- settings.py
+++ settings.py
@@ -1,13 +1,43 @@
 """Settings for Karmabot"""
 
+###  Files used by the bot ###
+
+# Path to the SQLite database file.  defaults to "db.sqlite3".
+# Note that this file will be created if it does not exist.
+# Additionally, the filename "db.sqlite3" is in the .gitignore file;
+# if you change it, be careful not to check it into version control.
 SQLITE_DB = "db.sqlite3"
+
+# Path to the file containing the Discord API key.
+# This file should contain only the API key, with no extra whitespace or newlines.
 DISCORD_API_KEY_FILE = "discordapikey.txt"
+
+
+### Buzzkill settings ###
+
+# Buzzkill settings are designed to curtail the more excessive enthusiasm
+# of users who might otherwise spam karma adjustments.
+
+# Maximum positive karma adjustment allowed in a single message.
 BUZZKILL_POSITIVE_MAX = 5
-# NOTE: Discord has a known issuer where more than two consecutive
-# minuses will be interpreted as a markdown horizontal rule.
+
+# Maximum negative karma adjustment allowed in a single message.
+# NOTE: Discord has a known issue where more than two consecutive
+# minuses will be interpreted as a Markdown horizontal rule.
 # Highly recommend keeping negative buzzkill at 2 to avoid this.
 BUZZKILL_NEGATIVE_MAX = 2
+
+
+### Anti-spam settings ###
+
+# Delay in seconds before a user can adjust karma again.
+# This applies to all users; i.e. if multiple users message @user +++,
+# within this delay, the user's karma will not be updated until the delay has passed.
+# This does not apply to one user adjusting multiple users' karma.
 KARMA_SPAM_DELAY = 15  # seconds
+
+
+## Utility functions for loading settings, do not adjust ###
 
 
 def load_api_key(api_key_file_path):
