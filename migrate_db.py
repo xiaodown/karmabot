ebc91f9add34dd017cbf8440c86538bbdc8661c4
Xiaodown <xiaodown@gmail.com>
2026-03-17 23:04:45 -0700

    don't need this anymore

--- migrate_db.py
+++
@@ -1,8 +0,0 @@
-"""Create or migrate the SQLite schema used by Karmabot."""
-
-from db import KarmaDatabase
-
-
-if __name__ == "__main__":
-    db = KarmaDatabase()
-    print(f"Database schema is ready at {db.db_path}")
